cdb8fe218e83cf2b47ff958111e55b76d27699a1
汪崎松 <wangqisong@live800.com>
2020-03-25 16:16:44 +0800

    close #1 lambda表达式书写可变参数方法

--- src/main/java/com/example/demo/test/LambdaTest.java
+++ src/main/java/com/example/demo/test/LambdaTest.java
@@ -9,6 +9,14 @@ public class LambdaTest {
     public static void main(String[] args) {
         Calculate1 c  = (a,b)->{return a+b; };
         System.out.println(c.add(1, 2));
+        Calculate2 c2  = (int a, int... b) ->{
+            int sum =a+0;
+            for (int i = 0; i <b.length ; i++) {
+                sum = sum+b[i];
+            }
+            return sum;
+        };
+        System.out.println(c2.add(1,2,3,4,5));
     }
 
     interface Calculate1{
@@ -16,6 +24,6 @@ public class LambdaTest {
     }
 
     interface Calculate2{
-        int add(int a ,int b);
+        int add(int a ,int... b);
     }
 }
